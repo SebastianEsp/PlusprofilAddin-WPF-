0266472efff1b36e5d8929114637eafc236b8463
Mathias Enggrob Boon <s144484@win.dtu.dk>
2017-11-28 17:26:12 +0100

    More work on ElementDialogViewModel

--- Plusprofil Add-in, WPF/Plusprofil Add-in/Plusprofil Add-in/ViewModels/DialogViewModel.cs
+++ Plusprofil Add-in, WPF/Plusprofil Add-in/Plusprofil Add-in/ViewModels/DialogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using EA;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace PlusprofilAddin.ViewModels
@@ -7,7 +8,56 @@ namespace PlusprofilAddin.ViewModels
     abstract class DialogViewModel
     {
         public Repository Repository { get; set; }
-
         abstract public void Initialize();
+
+        protected List<string> ToAddDanishTaggedValues = new List<string>
+        {
+            "prefLabel (da)",
+            "altLabel (da)",
+            "deprecatedLabel (da)",
+            "definition (da)",
+            "example (da)",
+            "comment (da)",
+            "applicationNote (da)"
+        };
+        protected List<string> ToAddEnglishTaggedValues = new List<string>
+        {
+            "prefLabel (en)",
+            "altLabel (en)",
+            "deprecatedLabel (en)",
+            "definition (en)",
+            "example (en)",
+            "comment (en)",
+            "applicationNote (en)"
+        };
+        protected List<string> ToAddProvenanceTaggedValues = new List<string>
+        {
+            "legalSource",
+            "source",
+            "isDefinedBy",
+            "wasDerivedFrom"
+        };
+
+        protected List<dynamic> RetrieveTaggedValues(List<dynamic> taggedValueList, string taggedValueName)
+        {
+            List<dynamic> result = new List<dynamic>();
+            if (taggedValueList.Count == 0) throw new Exception("No tagged values were found");
+            if(taggedValueList[0].ObjectType == (int)ObjectType.otRoleTag)
+            {
+                foreach (dynamic tv in taggedValueList)
+                {
+                    if (tv.Tag == taggedValueName) result.Add(tv);
+                }
+            }
+            else
+            {
+                foreach (dynamic tv in taggedValueList)
+                {
+                    if (tv.Name == taggedValueName) result.Add(tv);
+                }
+            }
+            if(result.Count == 0) throw new Exception("No tagged value with name " + taggedValueName + " was found");
+            return result;
+        }
     }
 }
--- Plusprofil Add-in, WPF/Plusprofil Add-in/Plusprofil Add-in/ViewModels/ElementDialogViewModel.cs
+++ Plusprofil Add-in, WPF/Plusprofil Add-in/Plusprofil Add-in/ViewModels/ElementDialogViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.ObjectModel;
 using EA;
 using PlusprofilAddin.ViewModels;
 using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace PlusprofilAddin
 {
@@ -10,6 +13,8 @@ namespace PlusprofilAddin
     {
         public Element Element { get; set; }
         public Collection TaggedValues { get; set; }
+        public List<dynamic> TaggedValuesList;
+
 
         public string URIValue { get; set; }
         public string UMLValue { get; set; }
@@ -26,6 +31,7 @@ namespace PlusprofilAddin
             EnglishTaggedValues = new ObservableCollection<PlusprofilTaggedValue>();
             ProvenanceTaggedValues = new ObservableCollection<PlusprofilTaggedValue>();
             StereotypeTaggedValues = new ObservableCollection<PlusprofilTaggedValue>();
+            TaggedValuesList = new List<dynamic>();
 
             URIValue = "";
             UMLValue = "";
@@ -38,23 +44,45 @@ namespace PlusprofilAddin
             UMLValue = Element.Name;
             AliasValue = Element.Alias;
             TaggedValues = Element.TaggedValues;
-            //TODO: Retrieve all tagged values in an efficient manner (i.e. do not use Collection.GetAt), store in a List<TaggedValue>
-
-            TaggedValue tv = TaggedValues.GetByName("URI");
-
-            URIValue = tv.Value;
+            //Retrieve all tagged values and store them in a list
+            //Tagged values are stored in a list to avoid iterating Collections multiple times, which is very costly
+            for (int i = 0; i < TaggedValues.Count; i++)
+            {
+                dynamic tv = TaggedValues.GetAt((short)i);
+                TaggedValuesList.Add(tv);
+            }
+            //Declare List to hold result of list lookups
+            List<dynamic> result;
+            result = RetrieveTaggedValues(TaggedValuesList, "URI");
+            URIValue = result[0].Value; //TODO: Create method to more elegantly retrieve object in single object list
 
             //Add all danish tagged values to list
-            DanishTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
+            foreach(string s in ToAddDanishTaggedValues)
+            {
+                result = RetrieveTaggedValues(TaggedValuesList, s);
+                foreach (dynamic tv in result) DanishTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
+            }
 
             //Add all english tagged values to list
-            EnglishTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
-
+            foreach (string s in ToAddEnglishTaggedValues)
+            {
+                result = RetrieveTaggedValues(TaggedValuesList, s);
+                foreach (dynamic tv in result) EnglishTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
+            }
             //Add all provenance tagged values to list
-            ProvenanceTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
-
+            foreach (string s in ToAddProvenanceTaggedValues)
+            {
+                result = RetrieveTaggedValues(TaggedValuesList, s);
+                foreach (dynamic tv in result) ProvenanceTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
+            }
             //Add all stereotype-specific tagged values to list
-            StereotypeTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
+            /*
+            foreach (string s in ToAddDanishTaggedValues)
+            {
+                tv = TaggedValuesList[s];
+                DanishTaggedValues.Add(new PlusprofilTaggedValue(tv.Name, tv.Value));
+            }
+            */
         }
     }
 }
